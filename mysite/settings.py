3aab0980331393845b1c42985189fc413a037e16
vkfkd1107 <ji_su1107@naver.com>
2023-01-31 18:13:42 +0900

    [Add] throttle settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,5 +130,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
-    ]
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '3/day'
+    }
 }
